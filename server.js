27458979f6850be01d605920b9f23f9c78f0d266
JML75 <jm.luquot@gmail.com>
2021-07-06 15:53:27 +0200

    creation de la route post et connexion à mongodb

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 // Require the framework and instantiate it
 const fastify = require('fastify')({ logger: true })
 
+
+// connection  fastify à la base de donnée 
+fastify.register(require('fastify-mongodb'), {
+    // force to close the mongodb connection when app stopped
+    // the default value is false
+    forceClose: true,
+
+    url: 'mongodb://localhost:27017/superheroes'
+
+})
+
 // cela chage le framework fastify dans la variable fastify ce qui devient un objet pour utiliser les methodes
 
 
@@ -19,11 +30,12 @@ fastify.get('/me', function() {
     return {
         prenom: "jim",
         nom: "Quinn",
-        job: "developpeur",
+        job: "developpeur WEB",
     }
 })
 
 // declarer la route heroes qui retourne un tableau
+// si on va sur ma route /heroes en get on consulte 
 fastify.get('/heroes', function() {
     const chanteurs = ['bowie', 'jaegger', 'guilmour']
     return chanteurs
@@ -31,6 +43,13 @@ fastify.get('/heroes', function() {
 
 
 
+//si on va sur /heroes en POST on ajoute un nouveau chanteur
+
+fastify.post('/heroes', (request, reply) => {
+    const collection = fastify.mongo.db.collection("heroes")
+    collection.insertOne(request.body)
+    return null
+})
 
 // Run the server!
 const start = async() => {
